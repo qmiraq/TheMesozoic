7dde3f678a8ef2dd0211ef10639fc122572b8a8d
Mira <neumannmiro0808@gmail.com>
2026-07-10 20:30:19 +0200

    Remove Visual Studio cache files from repository

--- bot/config/defaults.py
+++ bot/config/defaults.py
@@ -1,6 +1,9 @@
 DEFAULT_SETTINGS = {
     "server_name": "The Mesozoic",
-    "server_ip": "not_configured",
-    "server_port": "not_configured",
-    "status_channel": "not_configured"
+    "server_ip": "127.0.0.1",
+    "server_port": "7777",
+    "status_channel": "not_configured",
+    "server_host": "127.0.0.1",
+    "server_rcon_port": "8888",
+    "server_rcon_password": ""
 }
